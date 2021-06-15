9d99c37aeb408c2654d636274d78217de7ba14c0
darmiel <71837281+darmiel@users.noreply.github.com>
2021-06-16 00:13:57 +0200

    Added nasdaq

--- get_stock_names.py
+++ get_stock_names.py
@@ -1,8 +1,12 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 
-symbols = []
+symbols = {}
+
 for ch in "abcdefghijklmnopqrstuvwxyz":
+    char_symbols = []
+
     url = f"https://www.advfn.com/nasdaq/nasdaq.asp?companies={ch.upper()}"
     print("requesting", url, "...")
 
@@ -10,17 +14,22 @@ for ch in "abcdefghijklmnopqrstuvwxyz":
     soup = BeautifulSoup(resp.content, features='lxml')
 
     market_table = soup.find('table', class_='market')
-    char_symbols = []
     for row in market_table.find_all('tr'):
         td = row.find_all('td')
         if len(td) != 3:
             continue
-        symbol = td[1].string
-        symbols.append(symbol)
-        char_symbols.append(symbol)
-    print()
-    print("+ loaded", len(char_symbols), "for char", ch.upper())
-    print("- that's a total of", len(symbols), "symbols!")
+        
+        # read data
+        name = td[0].string.strip()
+        symbol = td[1].string.strip()
 
-print("symbols: (", len(symbols), "):")
-print(symbols)
+        if symbol in symbols:
+            print("")
+            print("WARN ::", symbol, "already in symbols!")
+            continue
+
+        symbols[symbol] = name
+
+    print("loaded:", len(symbols), "symbols")
+
+print(json.dumps(symbolsf))
