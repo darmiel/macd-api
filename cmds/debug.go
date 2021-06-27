6f2c152537bee93ab14c185a4bed15f0e9cab46e
darmiel <71837281+darmiel@users.noreply.github.com>
2021-06-27 16:21:04 +0200

    Added `yahoo#Historical`-Queries

---
+++ cmds/debug.go
@@ -0,0 +1,35 @@
+package cmds
+
+import (
+	"fmt"
+	"github.com/darmiel/macd-api/common"
+	"github.com/urfave/cli/v2"
+)
+
+func init() {
+	App.Commands = append(App.Commands, &cli.Command{
+		Name: "debug",
+		Action: func(ctx *cli.Context) (err error) {
+			db := common.MustPostgres()
+			fmt.Println("fetching symbols ...")
+			data, err := db.FindHistoricalsWithMinData(90)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("DATA:")
+			fmt.Println()
+			for k, v := range data {
+				fmt.Println(k, "::")
+				for _, a := range v {
+					fmt.Printf("%+v | ", a)
+				}
+				fmt.Println()
+				fmt.Println("  -> len:", len(v))
+				fmt.Println()
+			}
+			fmt.Println("len:", len(data))
+			return
+		},
+		Flags: []cli.Flag{},
+	})
+}
--- common/postgres.go
+++ common/postgres.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"fmt"
+	"github.com/darmiel/macd-api/yahoo"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -16,7 +17,11 @@ var (
 	PostgresTZ   = "Europe/Berlin"
 )
 
-func MustPostgres() *gorm.DB {
+type Postgres struct {
+	*gorm.DB
+}
+
+func MustPostgres() *Postgres {
 	var dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=%s",
 		PostgresHost, PostgresUser, PostgresPass, PostgresDb, PostgresPort, PostgresTZ)
 
@@ -26,5 +31,45 @@ func MustPostgres() *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
-	return db
+	return &Postgres{db}
+}
+
+func (p *Postgres) FindAllSymbols() (res []string, err error) {
+	tx := p.Model(&yahoo.Historical{}).
+		Distinct("symbol").
+		Order("symbol asc").
+		Find(&res)
+
+	err = tx.Error
+	return
+}
+
+func (p *Postgres) FindAllSymbolsWithMinData(num int) (res []string, err error) {
+	tx := p.Model(&yahoo.Historical{}).
+		Select("symbol").
+		Having("count(symbol) >= ?", num).
+		Group("symbol").
+		Order("symbol asc").
+		Find(&res)
+
+	err = tx.Error
+	return
+}
+
+func (p *Postgres) FindHistoricalsWithMinData(num int) (res map[string][]*yahoo.Historical, err error) {
+	h := make([]*yahoo.Historical, 0)
+	tx := p.Model(&yahoo.Historical{}).
+		Raw("SELECT * FROM historicals h WHERE symbol IN (SELECT symbol FROM historicals GROUP BY symbol HAVING COUNT(symbol) >= ?)", num).
+		Find(&h)
+	if err = tx.Error; err != nil {
+		return
+	}
+	res = make(map[string][]*yahoo.Historical)
+	for _, x := range h {
+		if _, o := res[x.Symbol]; !o {
+			res[x.Symbol] = make([]*yahoo.Historical, 0)
+		}
+		res[x.Symbol] = append(res[x.Symbol], x)
+	}
+	return
 }
