999f09cbcbf91b383b0e89c1c560e95764dd6bfd
darmiel <71837281+darmiel@users.noreply.github.com>
2021-06-29 12:35:28 +0200

    Added Dockerfile (WIP)

--- dev_postgres.sh
+++ dev_postgres.sh
@@ -7,3 +7,7 @@ docker run -d --rm \
     -v "$(pwd)/data/postgres:/var/lib/postgresql/data" \
     -p 45432:5432 \
     postgres
+
+# psql -d postgres -U postgres -c "COPY (${QUERY}) TO STDOUT WITH CSV HEADER DELIMITER E'\t'" | tr '.' ',' > /var/lib/postgresql/data/pgdata/output.csv
+# docker exec macds-api-postgres-dev psql -d postgres -U postgres -c "COPY (SELECT * FROM historicals LIMIT 10) TO STDOUT WITH CSV HEADER DELIMITER E'\t'" | tr '.' ','
+# docker build -f docker/update-90-days/Dockerfile .
